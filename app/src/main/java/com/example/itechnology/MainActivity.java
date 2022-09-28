49998ffc5733500089378bf33778f52f11394c76
CANCINO <bcancinog2@gmail.com>
2022-09-27 23:24:01 -0300

    xd

---
+++ app/src/main/java/com/example/itechnology/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.itechnology;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public EditText editCorreo, editContra;
+    public Button btn_login;
+    public String correo = "correo@correo.cl";
+    public String contrasena = "contra";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EnviarDatos();
+    }
+
+    void EnviarDatos(){
+        editCorreo = (EditText)findViewById(R.id.txtCorreo);
+        editContra = (EditText)findViewById(R.id.txtContrasena);
+        btn_login = (Button)findViewById(R.id.btnEnviar);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(editCorreo.getText().toString().equals(correo) && editContra.getText().toString().equals(contrasena)){
+                    Toast.makeText(MainActivity.this, "Usuario Correcto", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity.this,MainActivity3.class);
+                    intent.putExtra("correo", editCorreo.getText().toString()+"");
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(MainActivity.this, "Usuario Incorrecto", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/itechnology/MainActivity3.java
@@ -0,0 +1,72 @@
+package com.example.itechnology;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.itechnology.databinding.ActivityMain3Binding;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityMain3Binding binding;
+    String correo;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMain3Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Bundle b = getIntent().getExtras();
+        correo = b.getString("correo");
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        View headerView = navigationView.getHeaderView(0);
+        TextView textcorreo = (TextView) headerView.findViewById(R.id.txtCorreoUser);
+        textcorreo.setText(correo);
+
+        setSupportActionBar(binding.appBarMain.toolbar);
+        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        DrawerLayout drawer = binding.drawerLayout;
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                .setOpenableLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_activity3, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
